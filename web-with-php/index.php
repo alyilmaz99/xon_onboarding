f090566011681f675c8eb6acf73f2c5f6886c89a
alyilmaz99 <aliyilmaz9977@gmail.com>
2024-02-01 02:08:12 +0300

    add validator

--- web-with-php/index.php
+++ web-with-php/index.php
@@ -1,41 +1,66 @@
-<?php 
+<?php
 ini_set('display_errors', 1);
+ini_set('session.gc_maxlifetime', 10 * 60 * 60);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 
-if($_SERVER["REQUEST_METHOD"] == 'POST'){
-    $name = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $lastname = test_input($_POST["lastname"]);
-    $age = test_input($_POST["age"]);
+
+include_once 'Validator.php';
+
+$validator = Validator::getInstance();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $name = $validator->textValidation($_POST['name']);
+    $email = $validator->emailValidation($_POST["email"]);
+    $lastname =  $validator->textValidation($_POST['lastname']);
+    $age = $validator->ageValidation($_POST["age"]);
+    $website = $validator->websiteValidation($_POST["website"]);
+
+    if ($validator->isValid) {
+    }
 }
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-} 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Development With Php</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+
 </head>
+
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-    <label for="name">First name:</label><br>
-    <input type="text" id="name" name="name" value="Ali"><br>
-    <label for="lastname">Last name:</label><br>
-    <input type="text" id="lastname" name="lastname" value="Yilmaz"><br><br>
-    <label for="email">Email:</label><br>
-    <input type="text" id="email" name="email" value="okethis@gmail.com"><br>
-    <label for="age">Age:</label><br>
-    <input type="text" id="age" name="age" value="12"><br>
-    <input type="submit" value="Submit">
-    </form>
+    <div class="container ">
+        <div class="row">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <label for="name">First name:</label><br>
+                <input type="text" id="name" name="name" value="<?php echo isset($name['data']) ? $name['data'] : ''; ?>"><?php echo isset($name['isValid']) && $name['isValid'] === false ? $name['error'] : ""; ?><br>
+
+                <label for="lastname">Last name:</label><br>
+                <input type="text" id="lastname" name="lastname" value="<?php echo isset($lastname['data']) ? $lastname['data'] : ''; ?>"><?php echo isset($lastname['isValid']) && $lastname['isValid'] === false ? $lastname['error'] : ""; ?><br>
+
+                <label for="email">Email:</label><br>
+                <input type="text" id="email" name="email" value="<?php echo isset($email['data']) ? $email['data'] : ''; ?>"><?php echo isset($email['isValid']) && $email['isValid'] == false ? $email['error'] : ''; ?><br>
+
+                <label for="age">Age:</label><br>
+                <input type="text" id="age" name="age" value="<?php echo isset($age['data']) ? $age['data'] : ''; ?>"><?php echo isset($age['isValid']) && $age['isValid'] == false ? $age['error'] : '';  ?><br>
+
+                <label for="website">Website:</label><br>
+                <input type="text" id="website" name="website" value="<?php echo isset($website['data']) ? $website['data'] : ''; ?>"><?php echo isset($website['isValid']) && $website['isValid'] == false ? $website['error'] : ''; ?><br><br>
+                <input class="submit" type="submit" value="Submit">
+            </form>
+        </div>
+
+        </>
+
 </body>
+
 </html>
