cd64ba80f5ffe0d45b706e4913a80b76526a35b3
Ali Yilmaz <aliyilmaz9977@gmail.com>
2024-02-06 16:04:07 +0300

    init assigment mysql with php

---
+++ php-and-mysql/Database.php
@@ -0,0 +1,54 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+
+    protected string $host;
+    protected string $name;
+    protected string $user;
+    protected string $password;
+
+    private function __construct(
+        string $host,
+        string $name,
+        string $user,
+        string $password
+    ) {
+        $this->host = $host;
+        $this->name = $name;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public static function initialize(string $host, string $name, string $user, string $password)
+    {
+        if (!self::$instance) {
+            self::$instance = new self(
+                $host,
+                $name,
+                $user,
+                $password
+            );
+        }
+        return self::$instance;
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            throw new Exception("Database instance has not been initialized.");
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+}
---
+++ php-and-mysql/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once "./Database.php";
+$database = Database::Initialize("localhost", "cast_zone", "root", "");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
