6ee00eae060e6e2e9bd0dde67e02836063d23c17
alyilmaz99 <aliyilmaz9977@gmail.com>
2024-02-09 12:19:53 +0300

    test frm mc

--- web-with-php/index.php
+++ web-with-php/index.php
@@ -12,6 +12,7 @@ if (isset($_SESSION['name'])) {
 
 include_once 'Validator.php';
 
+
 $validator = Validator::getInstance();
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
@@ -40,9 +41,11 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Development With Php</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="<?php echo isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'dark' ?  "dark.css" : "style.css"; ?>">
+    <link rel="stylesheet"
+        href="<?php echo isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'dark' ?  "dark.css" : "style.css"; ?>">
 
 </head>
 
@@ -52,19 +55,24 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
                 <label for="name">First name:</label><br>
-                <input type="text" id="name" name="name" value="<?php echo isset($name['data']) ? $name['data'] : ''; ?>"><?php echo isset($name['isValid']) && $name['isValid'] === false ? $name['error'] : ""; ?><br>
+                <input type="text" id="name" name="name"
+                    value="<?php echo isset($name['data']) ? $name['data'] : ''; ?>"><?php echo isset($name['isValid']) && $name['isValid'] === false ? $name['error'] : ""; ?><br>
 
                 <label for="lastname">Last name:</label><br>
-                <input type="text" id="lastname" name="lastname" value="<?php echo isset($lastname['data']) ? $lastname['data'] : ''; ?>"><?php echo isset($lastname['isValid']) && $lastname['isValid'] === false ? $lastname['error'] : ""; ?><br>
+                <input type="text" id="lastname" name="lastname"
+                    value="<?php echo isset($lastname['data']) ? $lastname['data'] : ''; ?>"><?php echo isset($lastname['isValid']) && $lastname['isValid'] === false ? $lastname['error'] : ""; ?><br>
 
                 <label for="email">Email:</label><br>
-                <input type="text" id="email" name="email" value="<?php echo isset($email['data']) ? $email['data'] : ''; ?>"><?php echo isset($email['isValid']) && $email['isValid'] == false ? $email['error'] : ''; ?><br>
+                <input type="text" id="email" name="email"
+                    value="<?php echo isset($email['data']) ? $email['data'] : ''; ?>"><?php echo isset($email['isValid']) && $email['isValid'] == false ? $email['error'] : ''; ?><br>
 
                 <label for="age">Age:</label><br>
-                <input type="text" id="age" name="age" value="<?php echo isset($age['data']) ? $age['data'] : ''; ?>"><?php echo isset($age['isValid']) && $age['isValid'] == false ? $age['error'] : '';  ?><br>
+                <input type="text" id="age" name="age"
+                    value="<?php echo isset($age['data']) ? $age['data'] : ''; ?>"><?php echo isset($age['isValid']) && $age['isValid'] == false ? $age['error'] : '';  ?><br>
 
                 <label for="website">Website:</label><br>
-                <input type="text" id="website" name="website" value="<?php echo isset($website['data']) ? $website['data'] : ''; ?>"><?php echo isset($website['isValid']) && $website['isValid'] == false ? $website['error'] : ''; ?><br><br>
+                <input type="text" id="website" name="website"
+                    value="<?php echo isset($website['data']) ? $website['data'] : ''; ?>"><?php echo isset($website['isValid']) && $website['isValid'] == false ? $website['error'] : ''; ?><br><br>
                 <input class="submit" type="submit" value="Submit">
             </form>
         </div>
