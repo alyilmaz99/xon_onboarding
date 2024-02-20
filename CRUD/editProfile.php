cacfe5daa4819a2549919973b5533414b1b4e804
alyilmaz99 <aliyilmaz9977@gmail.com>
2024-02-20 15:47:51 +0300

    update jquery

--- CRUD/editProfile.php
+++ CRUD/editProfile.php
@@ -24,10 +24,11 @@ if (isset($_SESSION["id"])) {
             $time_start = microtime(true);
             header("refresh:3; url=index.php");
         } else if ($resp["status"] == true) {
+            header("Location: index.php");
         }
     } else if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["username"]) && isset($_POST["email"])) {
 
-        $url = "localhost/xon_onboarding/CRUD/api/updateUser/" . $_SESSION["id"];
+        $url = "localhost/xon_onboarding/CRUD/api/updateUser/" . $_POST["id"];
 
 
         $curl = curl_init();
@@ -55,6 +56,7 @@ if (isset($_SESSION["id"])) {
             $time_start = microtime(true);
             header("refresh:3; url=index.php");
         } else if ($resp["status"] == true) {
+            header("Location: index.php");
             echo "success";
         }
     } else {
@@ -110,20 +112,53 @@ if (isset($_SESSION["id"])) {
         }
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-    <h1>Hos Geldin <?= $resp["data"]["username"]; ?></h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <h1 id="title">Hos Geldin <span id="hello-username"></span></h1>
+    <form action="" method="post">
         <label for="username">UserName:</label><br>
-        <input type="text" id="username" name="username" value="<?= $resp["data"]["username"]; ?>"><br><br>
+        <input type="hidden" id="id" name="id" value=""><br><br>
+        <input type="text" id="username" name="username" value=""><br><br>
         <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" value="<?= $resp["data"]["email"]; ?>"><br><br>
+        <input type="email" id="email" name="email" value=""><br><br>
 
-        <input type="submit" class="btn fa-input" value="&#xf24d; Update">
+        <input id="editProfile" type="submit" class="btn fa-input" value="&#xf24d; Update">
 
     </form>
+
+    <script>
+        $(document).ready(function() {
+            $.get("api/user/<?= $_SESSION["id"] ?>", function(data, status) {
+                if (status == "success") {
+                    $('#username').val(data["data"]["username"]);
+                    $("#email").val(data["data"]["email"]);
+                    $("#hello-username").text(data["data"]["username"]);
+
+                }
+            });
+        });
+
+        $("#editProfile").click(function(event) {
+            event.preventDefault();
+            var userData = {
+                username: $("#username").val(),
+            };
+
+            $.ajax({
+                url: "api/updateUser/39",
+                type: "PUT",
+                contentType: "application/json",
+                data: JSON.stringify(userData),
+                success: function(data, status) {
+                    console.log(data)
+                    $("#hello-username").text(data["data"]);
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
