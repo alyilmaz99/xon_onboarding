2717ed7199498d77337ad8aa0603e35f5d9726d6
alyilmaz99 <aliyilmaz9977@gmail.com>
2024-02-27 20:15:40 +0300

    init blog ui

---
+++ blog/api/404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+    <style>
+    .center {
+        font-family: "Audiowide", sans-serif;
+        display: flex;
+        justify-content: center;
+        margin-right: auto;
+        align-items: center;
+        height: 100vh;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="center">
+        <h1>404</h1>
+        <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module">
+        </script>
+        <dotlottie-player src="https://lottie.host/272b60dd-462d-42a3-8ed6-fec4143633d6/X4FxBascRI.json"
+            background="transparent" speed="1" style="width: 500px; height: 500px" direction="1" playMode="normal" loop
+            autoplay></dotlottie-player>
+
+    </div>
+</body>
+
+</html>
---
+++ blog/app/index.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once 'vendor/autoload.php';
+
+const BASE_DIR = '/xon_onboarding/blog/app';
+const SRC_DIR = "/src";
+$request_uri = $_SERVER['REQUEST_URI'];
+if (substr($request_uri, 0, strlen(BASE_DIR)) === BASE_DIR) {
+    $request_uri = substr($request_uri, strlen(BASE_DIR));
+}
+
+$routes = [
+    '' => '/home/HomePage.php',
+    '/' => '/home/HomePage.php'
+
+];
+
+$matched = false;
+foreach ($routes as $route => $file) {
+    if (preg_match("~^$route$~", $request_uri, $matches)) {
+        if (strpos($route, ':') !== false) {
+            $file = preg_replace("~:$~", $matches[1], $file);
+        }
+        require __DIR__ . SRC_DIR . $file;
+        $matched = true;
+        break;
+    }
+}
+
+if (!$matched) {
+    http_response_code(404);
+    require __DIR__ . '/404.php';
+}
