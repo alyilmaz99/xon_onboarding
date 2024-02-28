2c3e501d09c3d5bd3de9d28efb215ad795531cc2
alyilmaz99 <aliyilmaz9977@gmail.com>
2024-02-28 12:00:15 +0300

    init home page

--- blog/app/src/home/HomePage.php
+++ blog/app/src/home/HomePage.php
@@ -5,10 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 </head>
 
 <body>
+    <div class="main">
+        <div class="navbar">
+            <div class="logo">
+                <p> Ali Yılmaz</p>
+            </div>
+            <ul class="navbar-ul" id="navbar-ul">
+                <li class="navbar-li"><a href="dashboard"> <span><img src="" class="nav-icon" /> </span>
+                        <span class="list-text">Home</span></a></li>
+                <li class="navbar-li"><a href="post"><span><img src="" class="nav-icon" /> </span>
+                        <span class="list-text">Blog</span></a></li>
+                <li class="navbar-li"><a href="category"><span><img src="" class="nav-icon" /></span>
+                        <span class="list-text">Category</span></a></li>
+                <li class="navbar-li"><a href="comments"><span><img src="" class="nav-icon" /> </span>
+                        <span class="list-text">Contact</span></a></li>
+                <li class="navbar-li"><a href="profile"><span><img src="" class="nav-icon" /> </span>
+                        <span class="list-text">Search</span></a></li>
+            </ul>
+        </div>
+        <div class="body">
+            <div class="left-column">
 
+            </div>
+            <div class="right-column">
+
+            </div>
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
 </body>
 
 </html>
